c519bc7b12b416b832353030b2cab200b0f8ba9c
dscacchetti <66273856+dscacchetti@users.noreply.github.com>
2020-06-10 16:01:51 -0400

    add hamburger icon animation
    added hamburger icon animation so that hamburger goes from icon to X

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 console.log("Hello! Welcome to my site! If you see something funky, let me know so I can fix it. :-)")
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+  var menuClick = document.getElementById("myNavtoggle");
+  if (menuClick.className === "navtoggle") {
+    menuClick.className += " responsive";
   } else {
-    x.className = 'navtoggle';
+    menuClick.className = "navtoggle";
   }
+}
+
+function menuIcon(x) {
+  x.classList.toggle("menu-close");
 }
