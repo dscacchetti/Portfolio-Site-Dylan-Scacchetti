380a24d2e592be70af2fcc87ba41b2cf291e0175
dscacchetti <66273856+dscacchetti@users.noreply.github.com>
2020-06-10 13:19:22 -0400

    add hamburger icon javascript
    added mobile menu with javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello! Welcome to my site! If you see something funky, let me know so I can fix it. :-)")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
